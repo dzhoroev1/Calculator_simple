26aa2f1a3f6348e8c2784cce538e03db606c52d5
Goguma <dzhoroev1@unist.ac.kr>
2019-09-16 10:27:51 +0900

    Initial Commit

---
+++ Calculator_simple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator_simple
+//
+//  Created by Temirlan Dzhoroev on 16/09/2019.
+//  Copyright © 2019 Timarc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
