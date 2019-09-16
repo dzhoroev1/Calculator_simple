f229042acfc39075a04d00e224a9994f1048d88d
Goguma <dzhoroev1@unist.ac.kr>
2019-09-16 11:28:03 +0900

    All code finalized
    fin

--- Calculator_simple/ViewController.swift
+++ Calculator_simple/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstNumber: UITextField!
+    
+    @IBOutlet weak var secondNumber: UITextField!
+    
+    var sum = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sumNumbers(_ sender: Any) {
+        sum = (Int(firstNumber.text!) ?? 0) + (Int(secondNumber.text!) ?? 0)
+        resultNumber.text = String(sum)
+    }
+    @IBAction func subtractNumber(_ sender: Any) {
+        sum = (Int(firstNumber.text!) ?? 0) - (Int(secondNumber.text!) ?? 0)
+        resultNumber.text = String(sum)
+    }
+    @IBAction func divideNumber(_ sender: Any) {
+        sum = (Int(firstNumber.text!) ?? 0) / (Int(secondNumber.text!) ?? 0)
+        resultNumber.text = String(sum)
+    }
+    @IBAction func multiplyNumber(_ sender: Any) {
+        sum = (Int(firstNumber.text!) ?? 0) * (Int(secondNumber.text!) ?? 0)
+        resultNumber.text = String(sum)
+    }
+    @IBAction func moduloNumber(_ sender: Any) {
+        sum = (Int(firstNumber.text!) ?? 0) % (Int(secondNumber.text!) ?? 0)
+        resultNumber.text = String(sum)
+    }
+    @IBOutlet weak var resultNumber: UILabel!
 }
 
